29f56498d64561025491e59b75b5535f9d788369
JamesPF <jamesfelz@gmail.com>
2016-10-24 07:02:05 -0700

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,20 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About Component</h1>
+      <p>This is a basic React application that allows the user to search for and view the current weather in a given location.</p>
+      <p>The main purpose of building this app was to get more familiar with components, routes, and Foundation.</p>
+      <p>Here are some of the tools I used.</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 };
 
