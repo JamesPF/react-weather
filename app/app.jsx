433d84989e9fb62bfa26ebf79b507e92521218c0
JamesPF <jamesfelz@gmail.com>
2016-10-24 06:30:01 -0700

    Add Foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
@@ -16,5 +20,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('app')
 );
-
-
